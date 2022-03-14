e4495f2fc94cc9652885eaa8c3d7c141f1d7d73e
abdellatif <ouldeabdellatif40@gmail.com>
2022-03-14 15:33:35 +0100

    add two rotes one for show th form of login and one for the action

--- src/main/java/controllers/AdminController.java
+++ src/main/java/controllers/AdminController.java
@@ -1,15 +1,49 @@
 package controllers;
 
+import entities.Admin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import service.AdminService;
+
+import javax.validation.Valid;
+import java.util.Map;
 
 
 @Controller
-public class UsrController {
+public class AdminController {
+
+    @Autowired
+    AdminService adminService;
 
    @GetMapping("/")
-    public String index(Model model) {
+    public String index() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String adminLogin(@Valid Admin admin, BindingResult result, Map model){
+
+        if (result.hasErrors()) {
+            return "login";
+        }
+
+        String email = "abdellatif@gmail.com";
+        String password = "12345";
+
+        admin = (Admin) model.get(admin);
+        if (!admin.getEmail().equals(email) || !admin.getPassWord().equals(password)){
+           return "login";
+        }
+
+        Boolean adminExists = adminService.isLoged(admin.getEmail(),admin.getPassWord());
+        if (adminExists){
+            return "redirect:/trainers";
+        }
 
 
         return "login";
