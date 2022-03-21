f9adbc8fbd781588773bd19bee21c2f07a829e90
abdellatif <ouldeabdellatif40@gmail.com>
2022-03-21 12:32:11 +0100

    create my own filter for login admin

--- src/main/java/controllers/SessionFilter.java
+++ src/main/java/controllers/SessionFilter.java
@@ -1,2 +1,34 @@
-package controllers;public class SessionFilter {
+package controllers;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/admin/*"})
+public class SessionFilter extends HttpFilter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("filter hhhhhhh");
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpSession session = request.getSession(false);
+       // String loginURI = request.getContextPath() + "/";
+
+        boolean loggedIn = session != null && session.getAttribute("email") != null;
+        if(loggedIn){
+            chain.doFilter(request, response);
+        }else {
+            request.getRequestDispatcher("login.html").forward(request, response);
+        }
+    }
 }
